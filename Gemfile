b252fb442f4c24cd16b9458c0a971a3fa79023ff
Alexandre Lairan <lairana@free.fr>
2020-08-04 21:04:32 +0200

    Add mailgun

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'dry-initializer'
 
 # misc
 gem 'whenever'
+gem 'mailgun_rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@hexagonal-coworking.com'
+  default from: 'no-reply@sandbox40a8d69737d04d6bb79d9368c833b511.mailgun.org'
   layout 'mailer'
 end
